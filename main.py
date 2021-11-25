0818bca77fb3568ba3d12ec4067d200c3c9d691c
Vetreria <90621103+Vetreria@users.noreply.github.com>
2021-11-25 07:40:37 +0300

    Исправления
    Убран код из ifmain
    Добавлены аргументы функций
    Убраны лишние переменные
    Добавлены шаблоны

--- main.py
+++ main.py
@@ -19,19 +19,19 @@ def shorten_link(token, url):
         headers={
           'Authorization': token}, json={"long_url": url, })
     response.raise_for_status()
-    return 'Битлинк ' + response.json()['link']
+    return 'Битлинк {0}'.format(response.json()['link'])
 
 
-def count_clicks(bitlink):
+def count_clicks(token, bitlink):
     response = requests.get(
         f'https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks/summary',
         headers={'Authorization': token}, params={'units': -1, }
     )
     response.raise_for_status()
-    return 'Всего кликов ' + str(response.json()['total_clicks'])
+    return 'Всего кликов {0}'.format(str(response.json()['total_clicks']))
 
 
-def is_bitlink(test_bitlink):
+def is_bitlink(test_bitlink, token):
     response = requests.get(
         f'https://api-ssl.bitly.com/v4/bitlinks/{test_bitlink}',
         headers={'Authorization': token})
@@ -43,26 +43,24 @@ def run(func):
 
 
 def main():
+    dotenv.load_dotenv()
+    token = os.getenv('BITLINK_TOKEN')
     parser = createParser()
     namespace = parser.parse_args(sys.argv[1:])
     url = namespace.link
     parsed = urlparse(url)
     test_bitlink = parsed.netloc + parsed.path
-    count_check = partial(count_clicks, parsed.netloc + parsed.path)
-    do_short = partial(shorten_link, token, url)
-    if is_bitlink(test_bitlink):
+    if is_bitlink(test_bitlink, token):
         try:
-            run(count_check)
+            run(partial(count_clicks, token, parsed.netloc + parsed.path))
         except requests.exceptions.HTTPError:
             exit('Ошибка подсчета кликов!')
     else:
         try:
-            run(do_short)
+            run(partial(shorten_link, token, url))
         except requests.exceptions.HTTPError:
             exit('Ошибка создания Битлинка!')
 
 
 if __name__ == '__main__':
-    dotenv.load_dotenv()
-    token = os.getenv('BITLINK_TOKEN')
     main()
